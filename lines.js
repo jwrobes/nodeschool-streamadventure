b2e1ff41cf665ef9c306c2a03d142f00f8fd191c
Jonathan Wrobel <jwrobes@gmail.com>
2014-07-03 14:13:35 -0700

    slightly refactored lines challenge

--- lines.js
+++ lines.js
@@ -1,9 +1,9 @@
 var through = require ('through');
 var split = require ('split');
-var count = 0;
+var lineCount = 0;
 var tr = through(function write(data) {
-	count ++;
-	if (count % 2 === 0) {
+	lineCount ++;
+	if (lineCount % 2 === 0) {
 		this.queue(data.toString().toUpperCase()+ "\n")
 	} else {
 		this.queue(data.toString().toLowerCase()+"\n")
